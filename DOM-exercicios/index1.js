7d0264dbace726c01761d2021e258ba64a9467e5
EMILLY MARRY DA SILVA JUVÊNCIO <emilly_juvencio@estudante.sesisenai.org.br>
2026-06-03 22:14:42 -0300

    DOM exercicios

---
+++ DOM-exercicios/index1.js
@@ -0,0 +1,8 @@
+const titulo = document.querySelector('#titulo');
+console.log(titulo.textContent);
+
+const paragrafo = document.querySelector('.descricao');
+console.log(paragrafo.textContent);
+
+const botao = document.querySelector('#btn');
+console.log(botao.tagName);
--- DOM/app.js
+++ DOM/app.js
@@ -46,5 +46,72 @@ btnAumentarJs.addEventListener('click', () => {
     barraJs.style.width = novoNivel + '%';
 });
 
+const btnEditarNome     = document.querySelector('#btn-editar-nome');
+const formEdicao        = document.querySelector('#form-edicao');
+const inputNome         = document.querySelector('#input-nome');
+const inputProfissao    = document.querySelector('#input-profissao');
+const nomeUsuario       = document.querySelector('#nome-usuario');
+const profissaoUsuario  = document.querySelector('#profissao-usuario');
 
+btnEditarNome.addEventListener('click', () => {
+   formEdicao.style.display = 'block';
+   
+   inputNome.value = nomeUsuario.textContent;
+   inputProfissao.value = profissaoUsuario.textContent;
+});
 
+const btnSalvar     = document.querySelector('#btn-salvar');
+const btnCancelar   = document.querySelector('#btn-cancelar');
+
+btnSalvar.addEventListener('click', () => {
+    const novoNome = inputNome.value.trim(); //trim tira oq n é necessario
+    const novaProfissao = inputProfissao.value.trim();
+
+    if(novoNome === ''){
+        alert('Nome vazio!');
+        return;
+    }
+
+    nomeUsuario.textContent = novoNome;
+    profissaoUsuario.textContent = novaProfissao || 'Sem profissão';
+
+    formEdicao.style.display = 'nome';
+});
+
+btnCancelar.addEventListener('click', () => {
+    formEdicao.style.display = 'none';
+});
+
+//trocar foto de perfil
+const btnTrocarFoto = document.querySelector('#btn-trocar-foto');
+const fotoOpcoes = document.querySelector('#foto-opcoes');
+const fotoPerfil = document.querySelector('#foto-perfil');
+
+btnTrocarFoto.addEventListener('click', () => {
+    if(fotoOpcoes.style.display === 'black'){
+        fotoOpcoes.style.display = 'none';
+    }else{
+        fotoOpcoes.style.display = 'block';
+        formEdicao.style.display = 'none';
+    }
+});
+
+const todasFotos = document.querySelectorAll('.foto-opcao');
+
+todasFotos.forEach(opcao => { //tipo um for mas faz auto
+
+    opcao.addEventListener('click', () => {
+
+        //<img dat-seed="Felix"> -> opcao.dataset.seed === "Felix"
+        const seed = opcao.dataset.seed; //dataset conjunto de dados
+
+        fotoPerfil.src = `https://api.dicebear.com/7.x/adventurer/svg?seed=${seed}`; //buscando na net as img
+
+        todasFotos.forEach(f = f.classList.remove('selecionada'));
+        opcao.classList.add('selecionada');
+
+        fotoOpcoes.style.display = 'none';
+
+        console.log('Avatar trocado para:', seed);
+    });
+});
